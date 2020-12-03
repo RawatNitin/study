5e926057bec7d226784d4b325fb77f43b75e9e07
RawatNitin <31441592+RawatNitin@users.noreply.github.com>
2020-12-03 07:31:18 +0530

    Update questions.js
    From GL Laptop

--- questions.js
+++ questions.js
@@ -7,9 +7,26 @@
 6. use-strict
 */
 
+https://developers.google.com/web/fundamentals/performance/critical-rendering-path/render-tree-construction
+https://itnext.io/reactjs-interview-questions-for-senior-developers-64618f6a0aca
+
+https://www.freecodecamp.org/news/server-side-rendering-your-react-app-in-three-simple-steps-7a82b95db82e/#:~:text=In%20Client%2Dside%20rendering%2C%20your,React%20components%20on%20the%20server.
+
+
 
 Create own split method
 Tree operations
 How webpack bundles files
 How lazyloading works
 permutations of String
+browser caching files
+cache busting
+preloading
+class vs function component: When to be used
+react memoisation:  when does it work, what are benefits
+BEM (Blocks Elements Modifiers)
+Repaint cycle
+less
+realtime app
+react app optimization
+
